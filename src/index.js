509d1b6e3daccbcc6db26c9cda1e01168b15282e
zahrary <zahrarostayar24@gmail.com>
2024-10-07 23:02:23 +0330

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,37 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#tempo");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let weatherDescription = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
 
   cityElement.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
-
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute}`;
+}
 function searchCity(city) {
   let apiKey = "be23329f1oeeef2ee4ea0141bf0ba2tf";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
